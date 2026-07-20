af274ee148d08b5b691718729cd552e188764476
kite1412 <natharaditya6@gmail.com>
2026-07-20 17:19:37 +0700

    fix(player): adjust player walk speed

--- scripts/player/PlayerMovementController.gd
+++ scripts/player/PlayerMovementController.gd
@@ -27,7 +27,7 @@ func process_movement() -> Vector2:
 		update_interaction_area_position()
 
 	player.is_sprinting = input_dir != Vector2.ZERO and Input.is_action_pressed("sprint")
-	var movement_speed = player.speed * 2.0 if player.is_sprinting else player.speed
+	var movement_speed = player.speed * 1.5 if player.is_sprinting else player.speed
 	player.velocity = input_dir * movement_speed
 	player.move_and_slide()
 	return input_dir
